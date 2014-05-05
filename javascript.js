8863d8b0edd6161237506cbbae43dc470b6e869d
Evan Pon <evanpon@gmail.com>
2014-05-04 18:34:12 -0700

    when logging in, save the value of the name for use later. clicking a card now places the vote above.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
   var websocket = initializeWebsocket();
+  var name = "";
   $("#start_button").click(function() {
-    var name = $("#name").val();
+    name = $("#name").val();
     var channel = $("#channel").val();
     var data = {path: "login/user", name: name, channel: channel}
     websocket.send(JSON.stringify(data));
+  })
+  $(".choice").click(function() {
+    var object = $("#" + name);
+    if (object.length > 0) {
+      var card = object.find(".card");
+      card.html($(this).html());
+    }
+    else {  
+      var html = "<li class='vote' id='";
+      html = html + name + "'><div class='card'>";
+      html = html + $(this).html();
+      html = html + "</div><div class='name'>"
+      html = html + name + "</div></li>";
+      $("#votes").append(html);
+    }
   })
 })
 function initializeWebsocket() {
