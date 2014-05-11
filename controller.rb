65cfcbc27d448ac5da73cf6a1faad8cbb68bf6c7
Evan Pon <evanpon@gmail.com>
2014-05-11 16:02:50 -0700

    add #broadcast for easy communication.

--- controller.rb
+++ controller.rb
@@ -17,16 +17,20 @@ class Controller
     user.name = message['name']
     
     Database.join_channel(user)
+    
     user.socket.send({login_successful: true}.to_json)
   end
   
   def vote
     user.vote = message['vote'].chomp
     user.save
-    user_ids = Database.channel(user.channel)
-    user_ids.each do |user_id|
-      channel_user = Database.user(user_id)
-      channel_user.socket.send({name: user.name, vote: user.vote}.to_json)
+    broadcast(user.channel, {name: user.name, vote: user.vote})
+  end
+  
+  def broadcast(channel, message)
+    Database.channel(channel).each do |user_id|
+      user = Database.user(user_id)
+      user.socket.send(message.to_json)
     end
   end
 end
